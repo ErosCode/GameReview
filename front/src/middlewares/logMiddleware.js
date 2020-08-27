bcbe6451654ee0690364d64c1c91fd9c203c6915
ErosCode <corentin.dc.eros@gmail.com>
2020-08-27 21:35:47 +0200

    add full front setup

--- front/src/middlewares/logMiddleware.js
+++ front/src/middlewares/logMiddleware.js
@@ -1,7 +1,7 @@
 // Just for you to know in which state you pass your action
 const logMiddleware = (store) => (next) => (action) => {
     console.log(store.getState());
-    console.log('Je laisse passer cette action: ', action);
+    console.log('I let pass this action: ', action);
     next(action);
   };
   
