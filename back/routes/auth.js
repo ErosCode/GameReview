ff42fd3f43ec235f0096bbc5b9efbc7e5de0e9ae
ErosCode <corentin.dc.eros@gmail.com>
2020-08-29 20:23:35 +0200

    add error on register

--- back/routes/auth.js
+++ back/routes/auth.js
@@ -5,10 +5,19 @@ const User = require('../model/User');
 const Joi = require('joi');
 
 const schema = Joi.object({
-    name: string
+    name: Joi.string().min(6).max(255).required(),
+    email: Joi.string().email().min(6).required(),
+    password: Joi.string().min(6).required()
 });
 
 router.post('/register', async (req, res) => {
+
+    // Validate the data before add a new user
+    const { error } = schema.validate(req.body);
+    if(error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
