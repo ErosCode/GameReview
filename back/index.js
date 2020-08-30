45aa90c36c851270491e6c4a2092e71bf6feee2b
ErosCode <corentin.dc.eros@gmail.com>
2020-08-31 00:59:59 +0200

    add token in headers and cors friendly

--- back/index.js
+++ back/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 // Import Routes
 const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts');
+
 
 dotenv.config();
 
@@ -16,9 +19,11 @@ mongoose.connect(
 );
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Route Middlewares
 app.use('/api/user', authRoute);
+app.use('/api/posts', postRoute);
 
 app.listen(3002, () => console.log('Server Up and Running'));
--- back/routes/auth.js
+++ back/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const { registerValidation, loginValidation }= require('../validation');
 
@@ -57,6 +58,11 @@ router.post('/login', async (req,res) => {
     if(!validPassword) {
         return res.status(400).send('Invalid password');
     };
+
+    // Create and assign a token
+    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+    res.header('auth-token', token).send(token);
+
     res.send('Logged In!');
 });
 
