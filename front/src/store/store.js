53dd7f9f258667d3b407f76ee803ad28e1841737
ErosCode <corentin.dc.eros@gmail.com>
2020-09-24 18:48:32 +0200

    fix middlewares call

--- front/src/store/store.js
+++ front/src/store/store.js
@@ -3,14 +3,14 @@ import { createStore, compose, applyMiddleware } from 'redux';
 
 // == Import : local
 import rootReducer from '../reducers';
-import logMiddleware from '../middlewares/logMiddleware';
+import Middlewares from '../middlewares';
 
 // == Enhancers
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const enhancers = composeEnhancers(
   applyMiddleware(
-    logMiddleware,
+    Middlewares,
     // secondMiddleware,
   ),
 );
