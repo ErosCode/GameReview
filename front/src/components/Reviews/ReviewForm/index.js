a998c9002e73a9477355cc979ddc754cc24b65f3
ErosCode <corentin.dc.eros@gmail.com>
2020-10-15 23:44:27 +0200

    add better review form

--- front/src/components/Reviews/ReviewForm/index.js
+++ front/src/components/Reviews/ReviewForm/index.js
@@ -10,24 +10,34 @@ const ReviewForm = () => (
           <label>
               Give your opinion about the game
           </label>
-          <textarea type="text" size="4" className="reviewForm__text"/>
-          <label>
-              Rate the graphics
-          </label>
-          <input type="number" />
-          <label>
-              Rate the scenario
-          </label>
-          <input type="number" />
-          <label>
-              Rate the mechanics
-          </label>
-          <input type="number" />
-          <label>
-              Rate the cutscenes
-          </label>
-          <input type="number" />
-          
+          <textarea type="text" size="4" minLength="20" maxLength="3200" className="reviewForm__text"/>
+          <div className="reviewForm__rates">
+          <div className="reviewForm__rate">
+            <label >
+                Rate the graphics
+            </label>
+            <input type="number" max="10" min="0"/>
+          </div>
+          <div className="reviewForm__rate">
+            <label>
+                Rate the story
+            </label>
+            <input type="number" max="10" min="0" />
+          </div>
+          <div className="reviewForm__rate">
+            <label>
+                Rate the writing
+            </label>
+            <input type="number" max="10" min="0" />
+          </div>
+          <div className="reviewForm__rate">
+            <label>
+                Rate the animation
+            </label>
+            <input type="number" max="10" min="0" />
+          </div>
+          </div>
+          <button className="reviewForm__submit">Send your review</button>
       </form>
   </div>
 );
