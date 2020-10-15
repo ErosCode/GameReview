95c2dcc83af1033785f883b04db4e9e09ef9daf7
ErosCode <corentin.dc.eros@gmail.com>
2020-10-16 01:16:12 +0200

    add footer css

--- front/src/components/Footer/index.js
+++ front/src/components/Footer/index.js
@@ -4,7 +4,37 @@ import PropTypes from 'prop-types';
 import './styles.scss';
 
 const Footer = () => (
-  <div className="footer">Footer</div>
+  <div className="footer">
+    <div className="footer__left">
+      <div>
+        Footer link
+      </div>
+      <div>
+        Footer link
+      </div>
+      <div>
+        Footer link
+      </div>
+      <div>
+        Footer link
+      </div>
+    </div>
+    <div className="footer__right">
+      <div>
+        Footer link
+      </div>
+      <div>
+        Footer link
+      </div>
+      <div>
+        Footer link
+      </div>
+      <div>
+        Footer link
+      </div>
+    </div>
+
+  </div>
 );
 
 export default Footer;
